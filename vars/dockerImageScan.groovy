c09a57dc093a23b9d2962bf5cce40e9b5fe1dd7b
wfgamal <wfahmy@gmail.com>
2023-11-09 08:27:09 +0100

    add-my-jenkinsfile-v5

--- vars/dockerImageScan.groovy
+++ vars/dockerImageScan.groovy
@@ -1,8 +1,8 @@
 def call (String USERHUB, String IMAGENAME){
 
    sh """
-   apt-get update
-   apt-get install trivy -y
+   sudo apt-get update
+   sudo apt-get install trivy -y
    trivy image ${USERHUB}/${IMAGENAME}:${BUILD_NUMBER} > scan.txt
    cat scan.txt
 
