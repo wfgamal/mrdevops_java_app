c111a1921ba830cba1fe0407ec79e6e9d5c987c5
wfgamal <wfahmy@gmail.com>
2023-11-09 07:51:20 +0100

    add-my-jenkinsfile-v5

--- vars/dockerImageScan.groovy
+++ vars/dockerImageScan.groovy
@@ -1,11 +1,11 @@
 def call (String USERHUB, String IMAGENAME){
 
    sh """
-   sudo apt-get install wget apt-transport-https gnupg lsb-release
-   wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo apt-key add -
-   echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list.d/trivy.list
-   sudo apt-get update
-   sudo apt-get install trivy -y
+   apt-get install wget apt-transport-https gnupg lsb-release
+   wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | apt-key add -
+   echo deb https://aquasecurity.github.io/trivy-repo/deb '$(lsb_release -sc)' main | tee -a /etc/apt/sources.list.d/trivy.list
+   apt-get update
+   apt-get install trivy -y
    trivy image ${USERHUB}/${IMAGENAME}:${BUILD_NUMBER} > scan.txt
    cat scan.txt
 
