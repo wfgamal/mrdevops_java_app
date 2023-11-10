5dd3289e844d94a243d50875e38979f5d08d2f14
wfgamal <wfahmy@gmail.com>
2023-11-10 09:04:21 +0100

    add-my-jenkinsfile-v5

--- vars/dockerImageScan.groovy
+++ vars/dockerImageScan.groovy
@@ -2,8 +2,8 @@ def call (String USERHUB, String IMAGENAME){
 
 
  // Install trivy
-                sh 'sudo chmod -R 777 /usr/bin'
-                sh 'curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | sh -s -- -b /usr/bin v0.18.3'
+                sh 'chown  angie:angie /usr/local/bin'
+                sh 'curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | sh -s -- -b /usr/local/bin v0.18.3'
                 sh 'curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/html.tpl > html.tpl'
 
    sh """
